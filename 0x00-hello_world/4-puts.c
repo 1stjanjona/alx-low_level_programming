912509e62c4df9fe966fd40db44b7ac823451ce5
1stjanjona <jannat.hammouche@outlook.com>
2023-08-10 16:00:36 -0700

    Puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,9 @@
 
 /**
  * main - Entry point
- *
  * Description: 'use puts instead of printf and return to the new line'
- * @parameter: 'Programming is like building a multilingual puzzle'
- *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
