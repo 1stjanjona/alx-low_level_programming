9729d6fe0939892c72e6cb2647e449e9ccf0f4c6
1stjanjona <jannat.hammouche@outlook.com>
2023-08-11 16:12:47 -0700

    Alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,9 +13,13 @@ int main(void)
 {
 	char alpha = 'a';
 
-	for (alpha != 'e' && alpha != 'q')
-		putchar(alpha);
+	while (alpha <= \z\)
+	{
+	if (alpha == 'e' || alpha == 'q')
 	alpha++;
+	putchar(alpha);
+	alpha++;
+	}
 	putchar('\n');
 
 	return (0);
