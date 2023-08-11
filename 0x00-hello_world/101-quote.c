14528ae81201c6e6f01fdc0882016615ccf38388
1stjanjona <jannat.hammouche@outlook.com>
2023-08-10 18:27:34 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Quote'
+ *
+ * Return: Always 1 (Fail)
+*/
+
+int main(void)
+{
+	string word = get_string("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
