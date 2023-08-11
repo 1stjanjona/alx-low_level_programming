d2927c493e6892bea75a48b58477997caecc7fc1
1stjanjona <jannat.hammouche@outlook.com>
2023-08-11 16:38:52 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'numbers of base 10'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("num%d", num);
+	return (0);
+}
