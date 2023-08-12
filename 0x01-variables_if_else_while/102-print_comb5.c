63d48ad27ed16de4ddc0fa5ef0f7dab5426b12ef
1stjanjona <jannat.hammouche@outlook.com>
2023-08-12 12:00:54 -0700

    Combination5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'combination5"
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = 1; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+			}
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
