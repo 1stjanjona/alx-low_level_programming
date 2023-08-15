fcef80b2e8a58b9db6337fdd123fb340e2f3eb5d
1stjanjona <jannat.hammouche@outlook.com>
2023-08-15 13:30:49 -0700

    is c alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for alphabetic character
+ *
+ * @c: 'is 'c' alpha'
+ *
+ * Return: 1 if 'c' is alpha otherwise Always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123 || c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+	return (0);
+}
