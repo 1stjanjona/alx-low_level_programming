fab43a39e2e5106cd0daf65a44408694c74e0207
1stjanjona <jannat.hammouche@outlook.com>
2023-08-15 16:15:57 -0700

    Every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,10 +22,11 @@ void jack_bauer(void)
 			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
-		}
-		min++;
-	}
-	hrs++;
 
-	_putchar('\n');
+			_putchar('\n');
+
+			min++;
+		}
+		hrs++;
+	}
 }
