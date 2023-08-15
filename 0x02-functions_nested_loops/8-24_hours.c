6c32f2de304d92858d692d368c1819555d989cd3
1stjanjona <jannat.hammouche@outlook.com>
2023-08-15 15:55:02 -0700

    Every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - check the day
+ *
+ * @hrs & @min: 'print every minute from 00:00 to 23:59'
+ *
+ * Return: no return, it's void
+*/
+
+void jack_bauer(void)
+{
+	int min;
+	int hrs;
+
+	for (hrs < 24 && hrs++)
+	{
+		for (min < 60 && min++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
