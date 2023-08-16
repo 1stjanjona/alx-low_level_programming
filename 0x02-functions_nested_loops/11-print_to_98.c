385e279b191fbc82ba2f7d8c53b155418212ad7e
1stjanjona <jannat.hammouche@outlook.com>
2023-08-16 05:40:05 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - check the numbers
+ *
+ * @n: 'print all n to 98'
+ *
+ * Return: No return
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (n = i; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+
+			else
+				printf("98\n");
+		}
+	}
+	else
+	{
+		for (n = i; i > 98; i--)
+			printf("%d, ", i);
+	}
+	printf("98\n");
+}
