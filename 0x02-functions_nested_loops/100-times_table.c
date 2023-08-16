9176e51e2924669c566713fdcd4c72323e1441e1
1stjanjona <jannat.hammouche@outlook.com>
2023-08-16 14:33:52 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,40 +10,30 @@
 
 void print_times_table(int n)
 {
-	int i;
+	int i, j;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
-				if (j == 0)
+				_putchar('m');
+				_putchar(' ');
+				if ((i * j) <= 9 && j != 0)
 				{
-					_putchar((i * j) + '0');
-				}
-				else if ((i * j) <= 9 && j != 0)
+					_putchar(' ');
+				} else if ((i * j) <= 99)
 				{
-					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((i * j) + '0');
-				}
-				else if ((i * j) <= 99 & (i * j) >= 10)
+				} else if ((i * j) >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(((i * j) / 10) + '0');
-					_putchar(((i * j) % 10) + '0');
-				}
-				else if ((n * i) >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar(((i * j) / 100) + '0');
 					_putchar((((i * j) / 10) % 10) + '0');
+				} else if ((i * j) <= 99 && (i * j) >= 10)
+				{
+					_putchar(((i * j) / 10) + '0');
 					_putchar(((i * j) % 10) + '0');
 				}
 			}
