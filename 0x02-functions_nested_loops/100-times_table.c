d4dd747ab6fcb5c3bdf1e2044643a1913a202742
1stjanjona <jannat.hammouche@outlook.com>
2023-08-16 14:40:16 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,11 +17,11 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			for (j = 1; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if ((i * j) <= 9 && j != 0)
+				if ((i * j) <= 9)
 				{
 					_putchar(' ');
 				} else if ((i * j) <= 99)
