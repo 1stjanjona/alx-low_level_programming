515ce9311061125d806c67d8192e31add9028526
1stjanjona <jannat.hammouche@outlook.com>
2023-08-16 16:28:04 -0700

    1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - add numbers
+ *
+ * Description: 'print, compute the sum'
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int a;
+	int sum = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		for ((a % 3) == 0 || (a % 5) == 0)
+		{
+			sum += a;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
