bac1bb191be0362d17a566b8fade82d6a48466a1
1stjanjona <jannat.hammouche@outlook.com>
2023-08-16 06:38:03 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,14 +21,14 @@ void print_to_98(int n)
 				printf("%d, ", i);
 
 			else
-				printf("%d\n", i);
+				printf("98\n");
 		}
 		for (i = n; i >= 98; i--)
 		{
 			if (i != 98)
 				printf("%d, ", i);
 			else
-				printf("%d\n", i);
+				printf("98\n");
 		}
 	}
 	else
@@ -38,5 +38,5 @@ void print_to_98(int n)
 			printf("%d, ", i);
 		}
 	}
-	printf("%d\n", i);
+	printf("98\n");
 }
