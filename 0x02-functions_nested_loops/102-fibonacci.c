d287d2907d85db9f1485b117631aa351bf4dccbd
1stjanjona <jannat.hammouche@outlook.com>
2023-08-16 16:54:38 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check Fibonacci code
+ *
+ * Description: 'print first 50'
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long fib, fibo, sum;
+
+	for (i = 0; i < 50; i++)
+	{
+		for (fib >= 0; fibo >= 1)
+		{
+			if (fib != fibo)
+			{
+				sum = fib + fibo;
+				printf("%lu", sum);
+			}
+			else if (fib == fibo)
+			{
+				sum = fibo;
+			}
+		}
+		if (i == 49)
+		{
+			printf("\n");
+		}
+		else
+			printf(",");
+	}
+	return (0);
+}
