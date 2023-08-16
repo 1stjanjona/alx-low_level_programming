e702aaac35e642798b673eda9c2444a8ec2dcd74
1stjanjona <jannat.hammouche@outlook.com>
2023-08-15 18:59:22 -0700

    add a to b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - check 'a' and 'b'
+ *
+ * @a: 'addition to b'
+ * @b: 'addition to a'
+ *
+ * Return: 'n'
+ */
+
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+
+	return (n);
+}
