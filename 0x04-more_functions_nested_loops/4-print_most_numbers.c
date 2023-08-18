1fa3825af9e0298f251c184eb57acc022d631490
1stjanjona <jannat.hammouche@outlook.com>
2023-08-18 13:34:52 -0700

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,9 +14,12 @@ void print_most_numbers(void)
 
 	for (a = 48; a <= 57; a++)
 	{
-		if (a != 50 && a != 52)
+		if (a != 50)
 		{
-			_putchar(a + '0');
+			if (a != 52)
+			{
+			_putchar(a);
+			}
 		}
 	}
 	_ptchar('\n');
