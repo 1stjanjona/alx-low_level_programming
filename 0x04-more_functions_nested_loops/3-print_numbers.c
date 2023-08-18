c8efd8ec303e0ba5ed06fa4d756ba2357ab189dd
1stjanjona <jannat.hammouche@outlook.com>
2023-08-18 10:42:29 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the integers
+ *
+ * Description: 'print numbers'
+ *
+ * Return: No Return.
+*/
+void print_numbers(void)
+{
+	for (a >= 48; a < 58)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
