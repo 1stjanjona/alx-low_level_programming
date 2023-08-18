62bc697ef460e9897c0f46a33a8ba312d4842894
1stjanjona <jannat.hammouche@outlook.com>
2023-08-18 14:18:54 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the numbers
+ *
+ * Description: 'print 10 times'
+ *
+ * Return: No return.
+*/
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i >= 0; i < 15, i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			_putchar("%d", i);
+		}
+	}
+	_putchar('\n');
+}
