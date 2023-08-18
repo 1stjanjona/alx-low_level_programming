d8d166e4bfd413bf306fb01b2402687136eb21a8
1stjanjona <jannat.hammouche@outlook.com>
2023-08-17 17:43:18 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -12,11 +12,12 @@ int _isupper(int c)
 {
 	if (c <= 65 && c >= 90)
 	{
-		_ptchar('%c: ');
+		_ptchar("%c: ");
 		return (1);
 	}
 	else
 	{
+		_ptchar("%c: ");
 		return (0);
 	}
 	_putchar('\n');
