20c88d80250f6adab2dd28f66dbf1cb59f48311a
1stjanjona <jannat.hammouche@outlook.com>
2023-08-18 10:04:57 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - check integers
+ *
+ * @a: 'multiply a to b'
+ * @b: 'myltiply b to a'
+ *
+ * Return: Always 0.
+*/
+int mul(int a, int b)
+{
+	_putchar("a * b");
+	_ptchar('\n');
+	return (0);
+}
