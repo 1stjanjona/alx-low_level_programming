08098807afb529f9f67daaf54b1901dd68e1cb0f
1stjanjona <jannat.hammouche@outlook.com>
2023-08-18 16:05:06 -0700

    commit main.h to push

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,10 +12,11 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
+			_putchar(j + '0');
 			if (j >= 10)
 			{
 				_putchar((j / 10) + '0');
@@ -23,5 +24,4 @@ void more_numbers(void)
 			}
 		}
 	}
-	_putchar('\n');
 }
