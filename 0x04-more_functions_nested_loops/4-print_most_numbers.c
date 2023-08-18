555dfbd021beaeffb7a32f4c50fc0c2dd8f9942c
1stjanjona <jannat.hammouche@outlook.com>
2023-08-18 12:00:10 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@
 
 void print_most_numbers(void)
 {
-	char a;
+	int a;
 
-	for (a >= '0'; a < '10'; a++)
+	for (a >= 48; a < 58; a++)
 	{
-		if (a != '2' || a != '4')
+		if (a != 50 || a != 52)
 		{
 			_putchar(a);
 		}
