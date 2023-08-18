ad4aefbef8d561b74d9d5d40c68d4041619b2f6c
1stjanjona <jannat.hammouche@outlook.com>
2023-08-18 11:20:09 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the numbers
+ *
+ * Description: 'print most numbers'
+ *
+ * Return: No Return.
+*/
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a >= 48; a < 58; a++)
+	{
+		if (a != 50 && a != 52)
+		{
+			_putchar(a);
+		}
+	}
+	_ptchar('\n');
+}
