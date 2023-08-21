d476d815d89a25c2f0751efcf47c2d6b18113fda
1stjanjona <jannat.hammouche@outlook.com>
2023-08-20 17:00:34 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,11 +14,11 @@ int largest_number(int a, int b, int c)
 
 	for (a != b || a != c || b != c)
 	{
-		if (a > b && b > c)
+		if (a > b && a > c)
 		{
 			largest = a;
 		}
-		else if (b > a && a > c)
+		else if (b > c)
 		{
 			largest = b;
 		}
