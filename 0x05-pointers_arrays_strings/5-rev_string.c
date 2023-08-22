5cbcea55a199db8a9a34cf14b8c0e77269fcb8f9
1stjanjona <jannat.hammouche@outlook.com>
2023-08-21 17:37:47 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - check the string
+ *
+ * @s: 'reverse a string'
+ *
+ * Return: No return.
+*/
+void rev_string(char *s)
+{
+	int i = 0;
+	int len;
+	char rev_s = s[0];
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		i--;
+		rev_s = s[i];
+		s[i] = s[len];
+		s[len] = rev_s;
+	}
+}
