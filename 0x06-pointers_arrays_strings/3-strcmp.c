162d3fedfdd10190650570bd51edc895afbf53fa
1stjanjona <jannat.hammouche@outlook.com>
2023-08-23 17:13:47 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,16 +6,25 @@
  * @s1: 'compare to s2'
  * @s2: 'compare to s1'
  *
- * Return: return(s1[i] - s2[i])
+ * Return: return(*s1 - *s2)
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int result;
 
-	while (s1[i] != s2[i])
+	result = ((int)*s1 - 48) - ((int)*s2 - 48);
+
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		return (s1[i] - s2[i]);
-		i++;
+		if (*s1 != *s2)
+		{
+			return (result);
+		}
+		s1++;
+		s2++;
 	}
-	return (0);
+	if (*s1 != '\0' || *s2 != '\0')
+		return (result);
+	else
+		return (0);
 }
