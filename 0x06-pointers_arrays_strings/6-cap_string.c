ff4e59f967409072b249e2b0dd2dce03e64c6a5e
1stjanjona <jannat.hammouche@outlook.com>
2023-08-25 15:46:43 -0700

    magic

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,39 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 /**
  * *cap_string - check the first letter
  *
  * @up: 'Capitalize the first letter only'
  *
- * Return: return (up)
+ * Return: return (pointer to up)
 */
 char *cap_string(char *up)
 {
 	int i = 0;
 	int j = 0;
-	char  c[j] = "\0 \t\n,;.!?\"(){}";
+	char sep[] = " \t\n,;.!?\"(){}";
+	char *ptr = up;
 
-	while (up[i])
+	while (*up)
 	{
-		while (up[i]  >= 'a' && up[i] <= 'z')
+		while (up[i] >= 'a' && up[i] <= 'z')
 		{
 			i++;
-			j++;
-			for (up[i] == c[j];)
+			for (j++; j < 12;)
 			{
-				i++;
-				up[i] -= 32;
+				if (up[i] == sep[j])
+				{
+					i++;
+					up[i] -= 32;
+				}
+				else
+					i++;
 			}
 		}
 		i++;
 	}
-	return (up);
+	return (ptr);
 }
