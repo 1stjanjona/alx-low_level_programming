dc66262dc6e5a4ef4f75566b1aaf94e6436ee8de
1stjanjona <jannat.hammouche@outlook.com>
2023-08-28 10:40:19 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_memcpy - check memory area
+ *
+ * @dest: 'memory area'
+ * @src: 'memory arae'
+ * @n: 'bytes to be coppied from src to dest'
+ *
+ * Return: return (dest).
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	while ((src[i] != '\0') && n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			dest[i] = src[i];
+		}
+		n--;
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
