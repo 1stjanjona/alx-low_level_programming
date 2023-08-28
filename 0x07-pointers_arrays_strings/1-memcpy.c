1f6b0938ca4e72b68b4eddf0df6f99bf76fa449d
1stjanjona <jannat.hammouche@outlook.com>
2023-08-28 10:48:00 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,12 +15,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	while ((src[i] != '\0') && n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			dest[i] = src[i];
 		}
 		n--;
 	}
-	dest[i++] = '\0';
 	return (dest);
 }
