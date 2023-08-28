2ff107504be9afc4a3330f1493d50d50dd5998cb
1stjanjona <jannat.hammouche@outlook.com>
2023-08-28 16:06:52 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,26 +4,29 @@
  * _strspn - check the length of a prefix sustring
  *
  * @s: 'number of bytes in the initial segment of s'
- * @accept: 's concicts only of bytes from accept'
+ * @accept: 's concists only of bytes from accept'
  *
  * Return: return the number of bytes in the initial segment of s.
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0;
-	unsigned int n;
+	unsigned int n = 0;
 
-	while (s[i] != '\0')
+	while (*s)
 	{
-		while (s[i] != accept[i] && n > 0)
+		while (*accept)
 		{
-			if (accept[i] == '\0')
+			if (*s == *accept)
+			{
+				n++;
+			}
+			else if (*accept != '\0')
 			{
 				return (n);
 			}
-			n++;
+			accept++;
 		}
-		i++;
+		s++;
 	}
 	return (n);
 }
