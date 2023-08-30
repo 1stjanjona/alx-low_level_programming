ca25737da74d0210d2d6f199dc587cfab91d4841
1stjanjona <jannat.hammouche@outlook.com>
2023-08-30 05:41:43 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print the string
+ *
+ * @s: 'the string to be printed'
+ *
+ * Return: No return.
+*/
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
