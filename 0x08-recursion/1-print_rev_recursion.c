bd01a2f477eec38e02a1758744ff3bbdf79ad594
1stjanjona <jannat.hammouche@outlook.com>
2023-08-30 08:15:03 -0700

    rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - check the string
+ *
+ * @s: 'print reversed string'
+ *
+ * Return: No return.
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
