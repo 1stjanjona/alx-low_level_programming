09fbcb2369e45b188ab0efe8b1a45e01b8806fea
1stjanjona <jannat.hammouche@outlook.com>
2023-09-05 14:55:09 -0700

    dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-/*
+/**
  * *_strdup - check the new duplicated string.
  *
- * @_strdup: 'duplicate string returned to new allocated memory'
  * @str: 'the string to be duplicated'
  *
  * Return: str = NULL or insufficient memory return (NULL),
