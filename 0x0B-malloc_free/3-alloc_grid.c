06b531a19dc0579f051cafa7aa8073393f548cf9
1stjanjona <jannat.hammouche@outlook.com>
2023-09-06 15:15:23 -0700

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,1 +1,53 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * **alloc_grid - check pointers of 2D array of integers.
+ *
+ * @width: 'width of the grid'
+ * @height: 'height of the grid'
+ *
+ * Return: on failure, for width or height <= 0 return (NULL)
+ * on Success return (ptr)
+*/
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (int **)malloc(sizeof(int *) * height);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i >= 0 && i < height; i++)
+	{
+		ptr[i] = (int *)malloc(sizeof(int) * width);
+
+		if (ptr[i] == NULL)
+		{
+			free(ptr[i]);
+			for (i = 0; j <= i; j++)
+			{
+				free(ptr[j]);
+			}
+			return (NULL);
+		}
+	}
+	for (i = 0; i >= 0 && i < height; i++)
+	{
+		for (j = 0; j < width && j >= 0; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
