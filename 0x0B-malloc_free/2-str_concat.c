30947d12f2e2fa5998818b5195ef496a0a64bf90
1stjanjona <jannat.hammouche@outlook.com>
2023-09-06 14:09:06 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,11 +17,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = NULL;
+		s1 = " ";
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
-		s2 = NULL;
+		s2 = " ";
 	}
 	while (s1[size1] != '\0')
 	{
