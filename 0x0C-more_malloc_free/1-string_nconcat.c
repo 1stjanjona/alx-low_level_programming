2adef8d12872c57b7045a496e31e202337b7e5e5
1stjanjona <jannat.hammouche@outlook.com>
2023-09-08 07:27:35 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,30 +13,28 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int size1 = 1, size2 = 1, size = 0, i = 0, j = 0;
+	unsigned int size1 = 1, size2 = 1, sum1 = 0, sum2 = 0, i = 0, j = 0;
 	char *str;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[size1] != '\0')
+	while (s1 && s1[size1] != '\0')
 	{
 		size1++;
 	}
-	while (s2[size2] != '\0')
+	while (s2 && s2[size2] != '\0')
 	{
 		size2++;
 	}
 	if (n >= size2)
 	{
 		n = size2;
+		sum1 = size1 + n;
+		str = malloc(sizeof(char) * (sum1 + 1));
 	}
-	size = size1 + n;
-	str = malloc(size + 1);
-
+	else if (n < size2)
+	{
+		sum2 = size1 + size2;
+		str = malloc(sizeof(char) * (sum2 + 1));
+	}
 	if (str == NULL)
 	{
 		return (0);
@@ -46,11 +44,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[i] = s1[i];
 		i++;
 	}
-	while (j <= n && s2[j] != '\0')
+	while ((n >= size2 && i < (size1 + n)) || (n < size2 && i < (size1 + size2)))
 	{
 		str[i] = s2[j];
 		i++, j++;
 	}
-	str[size] = '\0';
+	str[size1 + n] = '\0';
 	return (str);
 }
