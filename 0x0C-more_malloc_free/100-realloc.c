aad1ab0dec385b8189775b1e897f42775c010c30
1stjanjona <jannat.hammouche@outlook.com>
2023-09-09 16:08:19 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,14 +20,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (malloc(new_size));
 	}
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
 	}
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
@@ -38,7 +38,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		for (i = 0; i < old_size || i < new_size; i++)
 		{
-			if (new_size > old_size || new_size < old_size)
+			if (new_size != old_size)
 			{
 				new_ptr[i] = ((char *)ptr)[i];
 			}
