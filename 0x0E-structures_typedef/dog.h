a9e3d922b3236665325768dd2de803cd39a4d81c
1stjanjona <jannat.hammouche@outlook.com>
2023-09-11 13:29:46 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - ID of a dog
+ * @name: 'name of a dog'
+ * @age: 'age of a dog'
+ * @owner: 'owner of a dog'
+ *
+ * Return: Always 0.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
