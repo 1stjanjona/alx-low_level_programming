845c34837973d8947256bba1dc93878bfd61554d
1stjanjona <jannat.hammouche@outlook.com>
2023-09-14 14:43:44 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,10 +17,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(nmbrs, n);
 
-	if (separator == NULL)
-	{
-		printf("\n");
-	}
 	if (separator != NULL)
 	{
 		for (i = 0; i < n; i++)
@@ -30,6 +26,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		}
 	}
 	printf("\n");
-
 	va_end(nmbrs);
 }
