1a30b78ff01824b33bcf3cfa944c3a75358fb7e5
1stjanjona <jannat.hammouche@outlook.com>
2023-09-22 12:23:07 -0700

    winner

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  * Description: print this before main
  * Return: no return
 */
-void __attribute__((constructor)) bmain()
+void __attribute__ ((constructor)) bmain()
 {
 	printf("you're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,1 @@
-section .data
-    hello_fmt db "Hello, Holberton",10,0  ; The format string with newline
-
-section .text
-    global main
-    extern printf
-
-main:
-    ; Call printf function
-    mov rdi, hello_fmt
-    xor rax, rax              ; Clear RAX register (no xmm registers used, so no need for RAX = 0)
-    call printf
-
-    ; Exit program
-    mov rax, 60             ; syscall number for exit
-    xor rdi, rdi            ; exit status 0
-    syscall
+; ----------------------------------------------------------------------------------------; Writes "Hello, Holberton" to the console using only system calls. Runs on 64-bit Linux only.; To assemble and run:;;     nasm -felf64 101-hello_holberton.asm && ld 101-hello_holberton.o && ./hello; ----------------------------------------------------------------------------------------          global    _start          section   .text_start:   mov       rax, 1                  ; system call for write          mov       rdi, 1                  ; file handle 1 is stdout          mov       rsi, message            ; address of string to output          mov       rdx, 13                 ; number of bytes          syscall                           ; invoke operating system to do the write          mov       rax, 60                 ; system call for exit          xor       rdi, rdi                ; exit code 0          syscall                           ; invoke operating system to exit          section   .datamessage:  db        "Hello, Holberton", 10      ; note the newline at the end
