96d549bd7b8bd5b1a9441e48f0c9911fbf50629f
1stjanjona <jannat.hammouche@outlook.com>
2023-09-22 10:52:51 -0700

    winner

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@ void winner(void) __attribute__ ((constructor));
  * Description: print this before main
  * Return: no return
 */
-void winner(void) __attribute__ ((constructor))
+void winner(void)
 {
 	printf("you're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
