564d6c3d3c5d72ac10db900d3c83d3aaf23049a2
1stjanjona <jannat.hammouche@outlook.com>
2023-09-22 11:00:17 -0700

    winner

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
 
-void winner(void) __attribute__ ((constructor));
 /**
- * winner - print the winner
+ * bmain - print the winner
  * Description: print this before main
  * Return: no return
 */
-void winner(void)
+void __attribute__((constructor)) bmain()
 {
-	printf("you're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("you're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
