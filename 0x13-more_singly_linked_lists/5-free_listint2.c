011e936a9d6ecfbb048693a13f751c7bfeb932a1
1stjanjona <jannat.hammouche@outlook.com>
2023-09-25 15:31:17 -0700

    **head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,18 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current;
+	listint_t *pass;
 
 	if (head == NULL)
 	{
 		head = NULL;
 	}
-	while (*head != NULL)
+	current = *head;
+	while (current != NULL)
 	{
-		current = (*head)->next;
-		free(*head);
-		*head = current;
+		pass = current->next;
+		free(current);
+		current = pass;
 	}
+	*head = NULL;
 }
