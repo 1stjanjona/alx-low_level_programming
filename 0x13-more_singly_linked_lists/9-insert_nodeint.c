8147d528b74812fb2c63befca8864e162d62fd31
1stjanjona <jannat.hammouche@outlook.com>
2023-09-26 15:35:42 -0700

    idx node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * *insert_nodeint_at_index - insert a new node at a given position
+ * @head: double pointer to head list
+ * @idx: index of the new node
+ * @n: data n for the new node
+ * Return: return (new node), otherwise return (NULL)
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *pass;
+	listint_t *nsrt;
+	unsigned int x = 0;
+
+	nsrt = malloc(sizeof(listint_t));
+	if (nsrt == NULL || head == NULL)
+	{
+		return (NULL);
+	}
+	pass = *head;
+	nsrt->n = n;
+	nsrt->next = NULL;
+	if (idx == 0)
+	{
+		nsrt->next = pass;
+		pass = nsrt;
+		return (nsrt);
+	}
+	while (x < idx)
+	{
+		if (x == idx - 1)
+		{
+			nsrt->next = pass->next;
+			pass->next = nsrt;
+			return (nsrt);
+		}
+		x++;
+	}
+	return (nsrt);
+}
