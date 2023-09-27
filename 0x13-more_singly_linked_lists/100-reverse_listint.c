ddd7edee1c8454a153aef9ba9c66d44cf0d82f10
1stjanjona <jannat.hammouche@outlook.com>
2023-09-27 13:00:42 -0700

    reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * *reverse_listint - reverse list
+ * @head: double pointer to head list
+ * Return: return pointer to the first node,
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *rvrsd;
+	listint_t *pass;
+	listint_t *current;
+
+	current = *head;
+	while (current != NULL)
+	{
+		pass = current->next;
+		current->next = rvrsd;
+		rvrsd = current;
+		current = pass;
+	}
+	*head = rvrsd;
+	return (rvrsd);
+}
