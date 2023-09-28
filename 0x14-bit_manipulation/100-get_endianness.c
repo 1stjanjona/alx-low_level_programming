7b4dee79bdc2d3331996ba6d654bb7bb7851098f
1stjanjona <jannat.hammouche@outlook.com>
2023-09-28 10:37:30 -0700

    endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_endiannes - check endianness
+ * Return: return (0) if big endian, if little endian return (1)
+*/
+int get_endianness(void)
+{
+	unsigned int nmbr = 1;
+	char *byt = (char *)&nmbr;
+
+	if (*byt == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
