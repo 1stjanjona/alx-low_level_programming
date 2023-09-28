6811f55e597a0248c2cb20a3ac3d4b527a0ddea1
1stjanjona <jannat.hammouche@outlook.com>
2023-09-28 06:16:22 -0700

    uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert a binary number to uint
+ * @b: pointer to string
+ * Return: return (converted number), or return (0)
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int cnvrt = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b != NULL)
+	{
+		if (*b != '0' || *b != '1')
+		{
+			return (0);
+		}
+		cnvrt = cnvrt * 2 + (*b++ - '0');
+	}
+	return (cnvrt);
+}
