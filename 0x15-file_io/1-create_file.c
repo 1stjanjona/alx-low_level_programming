300b7bdb6ebdf9d0982afcc8b5dbae0656b07613
1stjanjona <jannat.hammouche@outlook.com>
2023-10-02 15:14:11 +0100

    again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,18 +11,27 @@ int create_file(const char *filename, char *text_content)
 	int out;
 	int lngth = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
-	while (text_content && text_content[lngth] != '\0')
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	while (text_content[lngth] != '\0')
 	{
 		lngth++;
 	}
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	out = write(fd, text_content, lngth);
-	if (fd == -1 || out == -1)
+	if (fd == -1)
 	{
+		return (-1);
+	}
+	out = write(fd, text_content, lngth);
+	if (out == -1)
+	{
+		close(fd);
 		return (-1);
 	}
 	close(fd);
