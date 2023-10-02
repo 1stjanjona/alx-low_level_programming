bf568781c38659a186a54d019019357613db61a2
1stjanjona <jannat.hammouche@outlook.com>
2023-10-02 14:16:57 +0100

    let it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,29 @@
 #include "main.h"
+/**
+ * read_textfile - read teaxt file to print POSIX
+ * @filename: file name
+ * @lettes: letters to read and print
+ * Return: return (0)
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    ssize_t bts, out;
+    char *bfr;
+    
+    if (filename == NULL || letters == NULL)
+    {
+        return (0);
+    }
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        return (0);
+    }
+    bfr = malloc(sizeof(char) * letters);
+    bts = read(fd, bfr, letters);
+    out = write(STDOUT_FILENO, bfr, bts);
+    free(bfr);
+    close(fd);
+    return (out);
+}
