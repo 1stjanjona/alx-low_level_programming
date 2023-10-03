e210aac9fd9024d21e2eb185b7bd56f62f7f4ebf
1stjanjona <jannat.hammouche@outlook.com>
2023-10-03 16:51:39 +0100

    commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -24,4 +24,8 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char *argv[]);
 
+int is_elf_file(const Elf64_Ehdr *elf_header);
+void display_elf_header_info(const Elf64_Ehdr *elf_header);
+void display_elf_header(const char *filename);
+
 #endif
