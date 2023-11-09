24bb6137fd41d5ad5e4fe8d03d30ab566ca56bb0
1stjanjona <jannat.hammouche@outlook.com>
2023-11-09 08:53:47 -0800

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		return (NULL);
+		return (add_dnodeint(h, n));
 	}
 	for (; idx != 1; idx--)
 	{
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (link->next == NULL)
 	{
-		return (NULL);
+		return (add_dnodeint_end(h, n));
 	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
