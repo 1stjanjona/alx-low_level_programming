be02c4380092f9a1903db7bf1568a96eaea75ca2
1stjanjona <jannat.hammouche@outlook.com>
2023-11-13 08:28:55 -0800

    updated

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,15 +1,41 @@
+#include <stdio.h>
+
+/**
+ * add - addition
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: addition of a & b
+*/
 int add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * sub - substracte
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: substracte
+*/
 int sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * mul - multiplication
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: multiplication
+*/
 int mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * div - division
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: division
+*/
 int div(int a, int b)
 {
 	while (b != 0)
@@ -17,6 +43,12 @@ int div(int a, int b)
 		return (a / b);
 	}
 }
+/**
+ * mod - modulu
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: modulu
+*/
 int mod(int a, int b)
 {
 	while (b != 0)
